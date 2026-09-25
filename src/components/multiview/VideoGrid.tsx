472f27dc7e47986618074f38af76b47879a1caf1
Exspieple <xx99593@gmail.com>
2026-09-25 15:36:04 +0200

    implement isHidden functionality

--- src/components/multiview/VideoGrid.tsx
+++ src/components/multiview/VideoGrid.tsx
@@ -7,20 +7,27 @@ import VideoGridAlertEmpty from "./VideoGridAlertEmpty";
 
 export default function VideoGrid() {
   const { videoData } = useContext(VideoDataContext);
+  const videoDataVisible = videoData.filter(({ isHidden }) => !isHidden);
 
-  const gridSize = Math.ceil(Math.sqrt(videoData.length + 1))
+  const gridSize = Math.ceil(Math.sqrt(videoDataVisible.length + 1));
 
-
-  if(videoData.length == 0) return <VideoGridAlertEmpty />
+  if (videoDataVisible.length == 0) return <VideoGridAlertEmpty />;
 
   return (
     <>
-      <div className="grid grid-cols-5 gap-0" style={{gridTemplateColumns: `repeat(${gridSize}, minmax(0, 1fr))`}}>
-        {videoData.map((_, index) => (
-          <VideoTileIndexContext key={index} value={index}>
-            <VideoTile />
-          </VideoTileIndexContext>
-        ))}
+      <div
+        className="grid grid-cols-5 gap-0"
+        style={{ gridTemplateColumns: `repeat(${gridSize}, minmax(0, 1fr))` }}
+      >
+        {videoData.map((video, index) => {
+          if (!videoDataVisible.includes(video)) return;
+
+          return (
+            <VideoTileIndexContext key={index} value={index}>
+              <VideoTile />
+            </VideoTileIndexContext>
+          );
+        })}
         <VideoControlTile />
       </div>
     </>
